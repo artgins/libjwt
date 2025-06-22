6bd26abbcfe06761dcae2a82aa5255fc8142ef5a
Rosa <rosa.martinez@artgins.com>
2025-06-23 01:47:35 +0200

    v3.2.1c fix cmake

--- configure.sh
+++ configure.sh
@@ -3,19 +3,27 @@
 CFLAGS="-Wno-error=char-subscripts -O3 -g -ggdb -fPIC"
 #CFLAGS+=" -I/lib/modules/$(uname -r)/build/include"
 export CFLAGS
-export PKG_CONFIG_PATH=/yuneta/development/outputs_ext/lib/pkgconfig
-export CC=musl-gcc
+export PKG_CONFIG_PATH=/yuneta/development/outputs_ext_static/lib/pkgconfig
+export CC=/usr/local/bin/musl-gcc
 export LDFLAGS="-static"
 
-rm -rf build
-mkdir build
-cd build
+export CFLAGS="-Wno-error=char-subscripts -O2 -DNDEBUG -fPIC"
+#export LDFLAGS="-static -no-pie"
 
+
+# rm -rf build
+mkdir -p build
+cd build
+rm -rf *
+
+cp ../include/jwt_export.h .
 cmake \
-    -DCMAKE_INSTALL_PREFIX:PATH="/yuneta/development/outputs_ext" \
+    -DCMAKE_INSTALL_PREFIX:PATH="/yuneta/development/outputs_ext_static" \
     -DEXCLUDE_DEPRECATED=TRUE \
     -DWITH_GNUTLS=OFF \
     -DWITH_MBEDTLS=ON \
     ..
+
+cp ../include/jwt_export.h .
 
 make install
